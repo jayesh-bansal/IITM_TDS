5858f7532af42207541802863d0c256866c87104
JB <143644724+jayesh-bansal@users.noreply.github.com>
2025-02-02 16:33:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api")
-def read_root():
-    return {"message": "Hello, World!"}
-
-@app.get("?name={marks1}&?name={marks2}")
+@app.get("/api?name={marks1}&?name={marks2}")
 def marks():
     return {"marks": [marks1, marks2] )
