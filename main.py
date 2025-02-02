7cc9c154e06808a8cb3a06a1a2aa735e9294cf1f
JB <143644724+jayesh-bansal@users.noreply.github.com>
2025-02-02 16:36:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api?name={marks1}&?name={marks2}")
+@app.get("/api?name={marks1}&name={marks2}")
 def marks():
     return {"marks": [marks1, marks2] )
